1ca861fb1c8a971efbed95d6bdb30a800d919767
Karl Ehatäht <karlehataht@gmail.com>
2018-05-09 19:22:02 +0300

    Possibility to pass arguments to the lambda functions via command line
    This feature is needed when we want to pass PU profiles that depend on a particular MC sample to puWeightProducer. Example:
    
    'someLambdaFunction,lambdaFunctionWithArguments(arg1;arg2;arg30,anotherLambdaFunction'
    
    The second lambda is automatically bound w/ the arguments provided in the parentheses (in the same order as specified). In other words, the underlying object which the lambda functions instantiate (whether it be bound or unbound) are created with the arguments provided by the user. If there are no arguments (as is the case for the 1st and the 3rd lambda), the default behavior kicks in.
    
    Tested.

--- scripts/nano_postproc.py
+++ scripts/nano_postproc.py
@@ -5,6 +5,19 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 from importlib import import_module
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 
+def find_modules(names):
+  split = names.split(',')
+  has_args = lambda name: name.find('(') != -1 and name.endswith(')') and name.count('(') == 1 and name.count(')') == 1
+  modules = {}
+  for name in split:
+      if has_args(name):
+          module_name = name[:name.find('(')]
+          module_args = tuple(name[ name.find('(') + 1 : name.find(')') ].split(';'))
+          modules[module_name] = module_args
+      else:
+          modules[name] = None
+  return modules
+
 if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] outputDir inputFiles")
@@ -15,7 +28,7 @@ if __name__ == "__main__":
     parser.add_option("--friend",  dest="friend", action="store_true", default=False, help="Produce friend trees in output (current default is to produce full trees)")
     parser.add_option("--full",  dest="friend", action="store_false",  default=False, help="Produce full trees in output (this is the current default)")
     parser.add_option("--noout",  dest="noOut", action="store_true",  default=False, help="Do not produce output, just run modules")
-    parser.add_option("--justcount",   dest="justcount", default=False, action="store_true",  help="Just report the number of selected events") 
+    parser.add_option("--justcount",   dest="justcount", default=False, action="store_true",  help="Just report the number of selected events")
     parser.add_option("-I", "--import", dest="imports",  type="string", default=[], action="append", nargs=2, help="Import modules (python package, comma-separated list of ");
     parser.add_option("-z", "--compression",  dest="compression", type="string", default=("LZMA:9"), help="Compression: none, or (algo):(level) ")
 
@@ -30,17 +43,23 @@ if __name__ == "__main__":
     outdir = args[0]; args = args[1:]
 
     modules = []
-    for mod, names in options.imports: 
+    for mod, names in options.imports:
         import_module(mod)
         obj = sys.modules[mod]
-        selnames = names.split(",")
+        selnames = find_modules(names)
         for name in dir(obj):
             if name[0] == "_": continue
+
             if name in selnames:
-                print "Loading %s from %s " % (name, mod)
-                modules.append(getattr(obj,name)())
+                print "Loading %s from %s%s" % (name, mod, (' with arguments: %s' % ', '.join(selnames[name]) if selnames[name] else ''))
+                imported_func = getattr(obj,name)
+                if selnames[name]:
+                    bound_func = lambda: imported_func(*selnames[name])
+                    modules.append(bound_func())
+                else:
+                    modules.append(imported_func())
     if options.noOut:
-        if len(modules) == 0: 
+        if len(modules) == 0:
             raise RuntimeError("Running with --noout and no modules does nothing!")
     p=PostProcessor(outdir,args,options.cut,options.branchsel,modules,options.compression,options.friend,options.postfix,options.json,options.noOut,options.justcount)
     p.run()
