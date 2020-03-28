7b136b00a777a0e7c3e9ebac0abb85a4608aad6d
ktht <karlehataht@gmail.com>
2020-03-28 16:27:17 +0200

    Undo JEC in SD mass recomputation only if it's possible
    Unfortunately, it turns out that the AK8LS subjet collection does not store the factor that recovers the raw energy of the subjets as required in SD mass computation. However, it's not clear to me if the subjets that we've created for the custom AK8LS collection have JEC applied or not.
    Somewhat related: https://github.com/HEP-KBFI/tth-htt/issues/136

--- python/postprocessing/modules/jme/fatJetUncertainties.py
+++ python/postprocessing/modules/jme/fatJetUncertainties.py
@@ -348,7 +348,9 @@ class fatJetUncertaintiesProducer(Module):
                 if jet.subJetIdx1 >= 0 and jet.subJetIdx2 >= 0 :
                     subJets1 = subJets[jet.subJetIdx1]
                     subJets2 = subJets[jet.subJetIdx1]
-                    groomedP4 = subJets1.p4() * (1. - subJets1.rawFactor) + subJets2.p4() * (1. - subJets2.rawFactor)
+                    subJets1_rawFactor = subJets1.rawFactor if hasattr(subJets1, 'rawFactor') else 0.
+                    subJets2_rawFactor = subJets2.rawFactor if hasattr(subJets2, 'rawFactor') else 0.
+                    groomedP4 = subJets1.p4() * (1. - subJets1_rawFactor) + subJets2.p4() * (1. - subJets2_rawFactor)
                 else :
                     groomedP4 = None
                 
